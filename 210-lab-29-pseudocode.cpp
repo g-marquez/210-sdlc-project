5679503de99afbcd4e130cd07af7a03c2a7cf1f1
Gabe Marquez <gabrielmarquez98@gmail.com>
2026-04-14 18:21:12 -0700

    update

--- 210-lab-29-pseudocode.cpp
+++ 210-lab-29-pseudocode.cpp
@@ -1,4 +1,16 @@
 //*****************************************************************************
 // COMSC-210 | Lab 29 | Gabriel Marquez
-// Description: this file 
+// Description: this file outlines the major steps and blocks of code necessary
+// for the language evolution simulation SDLC project.
 //*****************************************************************************
+
+//Include necessary headers for file handling, data structures, etc.
+#include <iostream>
+#include <map>
+#include <list>
+#include <array>
+#include <fstream>
+#include <random>
+using namespace std;
+
+//Declare function prototype(s) for different 
